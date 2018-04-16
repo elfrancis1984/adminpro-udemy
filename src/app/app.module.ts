25d04ac2e316ab08087154494a3328933ac7d7e3
Francis <francischalan@gmail.com>
2018-04-16 18:35:39 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,9 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTES } from './app.routes';
 // Modules
 import { PagesModule } from './pages/pages.module';
-
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-// import { DashboardComponent } from './pages/dashboard/dashboard.component';
-// import { ProgressComponent } from './pages/progress/progress.component';
-// import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -24,14 +16,6 @@ import { RegisterComponent } from './login/register.component';
     AppComponent,
     LoginComponent,
     RegisterComponent
-    // DashboardComponent,
-    // ProgressComponent,
-    // Graficas1Component,
-    // PagesComponent,
-    // NopagefoundComponent,
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumbsComponent,
   ],
   imports: [
     BrowserModule,
